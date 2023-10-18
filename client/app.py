4edce2bf2f34fb71a9ee3bb8b1a2a6f8e920efd4
Pouria Mistani <pmistani@nvidia.com>
2023-10-18 15:59:26 -0700

    added server and clinet code using generated gRPC from protobuf

---
+++ client/app.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+import logging
+
+import grpc
+import users_pb2
+import users_pb2_grpc
+
+
+def run():
+    response = []
+    print("Will try to greet world ...")
+    with grpc.insecure_channel("localhost:50051") as channel:
+        stub = users_pb2_grpc.UsersStub(channel)
+        response = stub.GetUsers(users_pb2.GetUsersRequest())
+    print(response.users)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    run()
