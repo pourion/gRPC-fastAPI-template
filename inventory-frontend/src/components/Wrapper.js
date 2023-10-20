87e2e5ba41784dee88f629251de1a93d356012ee
Pouria Mistani <pmistani@nvidia.com>
2023-10-19 22:08:25 -0700

    refactoring the web app frontend

---
+++ inventory-frontend/src/components/Wrapper.js
@@ -0,0 +1,34 @@
+export const Wrapper = props => {
+    return < >
+        <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+            <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Company name</a>
+        </header>
+
+        <div class="container-fluid">
+            <div class="row">
+                <div class="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary">
+                    <div class="offcanvas-md offcanvas-end bg-body-tertiary" tabindex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="sidebarMenuLabel">Company name</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebarMenu" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
+                            <ul class="nav flex-column">
+                                <li class="nav-item">
+                                    <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                                        {/* <svg class="bi"><use xlink:href="#house-fill" /></svg> */}
+                                        Products
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    {props.children}
+                </main>
+            </div>
+        </div>
+    </>
+}
